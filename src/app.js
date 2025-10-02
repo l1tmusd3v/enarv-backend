e2af350d6dc2abe6b1c29e29388d7470763604d8
l1tmusd3v <l1tmusd3v@gmail.com>
2025-10-02 08:38:21 +0530

    Update deployment

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const cors = require('cors');
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const router = require('./api/routes');
 
 app.use('/api', router);
-app.use(cors());
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
