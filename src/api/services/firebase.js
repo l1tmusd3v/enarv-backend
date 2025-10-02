3ffdcb4ab9406d3784ff579b73f82cdaa6c9bf03
l1tmusd3v <l1tmusd3v@gmail.com>
2025-10-02 08:31:48 +0530

    Update Firebase

--- src/api/services/firebase.js
+++ src/api/services/firebase.js
@@ -1,5 +1,9 @@
 const admin = require('firebase-admin');
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG || '{}');
+if (!process.env.FIREBASE_CONFIG) {
+    console.error('FIREBASE_CONFIG environment variable is not set');
+    process.exit(1);
+}
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
 admin.initializeApp({
